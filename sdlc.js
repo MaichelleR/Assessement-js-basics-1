d2fba9ffbba6ef5acf893f3fa9785e2382eaaea3
unknown <maichellereynolds2@gmail.com>
2022-11-21 18:15:29 -0800

    Completed test

---
+++ sdlc.js
@@ -0,0 +1,34 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation                  
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+  
+// PLANNING
+// ANALYSIS OF REQUIREMANTS 
+// DESIGN 
+// IMPLEMENTATION
+// TESTING 
+// MAINTAIN 
+ 
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirmants = "This is the second step in the process.Software Analysis is the process of collating factual data, identify software flaws, understanding the information architecture and developing solutions to overcome the weaknesses of the project. "
+const design ="This is the third step in the process. Here you will consider the technical requirements and propose multiple designing approachs. Additionally,document the desings specifications that are need for the software."
+const implementation = "This is the fourth step in the process.This phase requires developers to implement what has been decided in the previous phases. The developers build the code, test, integrate and manage. "
+const testing = "This the the fifth step in the process. In this stage Testing is an important phase of the software development life cycle. It helps to find the bugs and errors in the software. "
+const maintain = "This is the sixth step in the process. In this step software is released successfully in the market, and maintance and updates are done on the software."
+ 
